1bbdf95ddcf0055abd1443bdd092412ed7c34270
Trias Ahmad Fairuz <trias3011@gmail.com>
2022-09-14 11:30:48 +0700

    finished MVT implementation

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,10 @@
 # TODO: Implement Routings Here
+
+from django.urls import path
+from katalog.views import show_catalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_catalog, name='show_catalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_item' : data_catalog_item,
+    'name' : 'Trias Ahmad Fairuz',
+    'id' : '2106633645'
+
+}
+
+def show_catalog(request):
+    return render(request, 'katalog.html', context)
 
 # TODO: Create your views here.
