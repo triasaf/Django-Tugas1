60d18989f94dc65ff9fd8495e910e45a5f086672
Trias Ahmad Fairuz <trias3011@gmail.com>
2022-10-06 10:31:36 +0700

    Tugas5: styling menggunakan bootstrap, mengubah textField() menjadi dateField() utk models

--- todolist/models.py
+++ todolist/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.TextField()
+    date = models.DateField()
     title = models.TextField()
     description = models.TextField()
     is_finished = models.BooleanField(default=False)
